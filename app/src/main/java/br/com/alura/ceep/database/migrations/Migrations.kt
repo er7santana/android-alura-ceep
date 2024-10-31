806ca3738791170788e9fd96ee050e77bfed8661
Eliezer Sant Ana <er.7santana@gmail.com>
2024-10-31 03:31:11 -0300

    adicionando flag de sincronizacao em Nota

--- app/src/main/java/br/com/alura/ceep/database/migrations/Migrations.kt
+++ app/src/main/java/br/com/alura/ceep/database/migrations/Migrations.kt
@@ -43,3 +43,14 @@ val MIGRATION_1_2 = object : Migration(1, 2) {
         database.execSQL("ALTER TABLE $tabelaNova RENAME TO $tabelaAtual")
     }
 }
+
+val MIGRATION_2_3 = object : Migration(2, 3) {
+    override fun migrate(database: SupportSQLiteDatabase) {
+        database.execSQL(
+            """
+        ALTER TABLE Nota 
+            ADD COLUMN sincronizada INTEGER NOT NULL DEFAULT 0
+    """
+        )
+    }
+}
